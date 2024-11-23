6f2eb21cb57bc2fafa2e9dc4a0f9b0e57206a43c
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-11-23 15:38:25 -0300

    feat: implement the datasets preprocessing

---
+++ etl.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def preprocess_localiza_data(carros_csv: str, vendas_csv: str):
+    carros_df = pd.read_csv(carros_csv, delimiter=";").dropna()
+
+    vendas_df = pd.read_csv(vendas_csv, delimiter=";", low_memory=False).dropna()
+
+    final_df = pd.merge(
+        vendas_df, carros_df, left_on="MODELO", right_on="MODELO"
+    ).rename(columns=lambda x: x.lower().replace(" ", "_"))
+
+    final_df["modelo"] = final_df["modelo"].str.lstrip("MODELO")
+    final_df["marca"] = final_df["marca"].str.lstrip("MARCA")
+
+    return final_df
